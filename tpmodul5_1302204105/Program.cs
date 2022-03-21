64feee7ad282e1530bfa69c0d06be245ba44b063
OrvalaAM <orvalavala@gmail.com>
2022-03-22 01:13:35 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302204105/Program.cs
+++ tpmodul5_1302204105/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302204105
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Orvala");
+            video.IncreasePlayCount(50);
+            video.PrintVideoDetails();
+        }
+    }
+}
---
+++ tpmodul5_1302204105/SayaTubeVideo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302204105
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo() { }
+        public SayaTubeVideo(string judul)
+        {
+            title = judul;
+            Random rnd = new Random();
+            id = rnd.Next(10000, 100000);
+            playCount = 0;
+        }
+        public void IncreasePlayCount(int jumlah)
+        {
+            playCount = playCount + jumlah;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID       :" + id);
+            Console.WriteLine("Judul    :" + title);
+            Console.WriteLine("Views    :" + playCount);
+        }
+    }
+}
